9ed8c1ef986720e48b146b86e7b8dff86f26c9e8
Adam Maracz <adam.maracz@me.com>
2022-04-19 18:36:45 +0200

    Added missing Source and Type attributes

--- convert.sh
+++ convert.sh
@@ -47,7 +47,7 @@ for id in `jq '.[].id' archivedAppData.json`; do
         echo "Maintainer: $author"
         echo "Size: $appSize_appjson"
         echo "Filename: $filename_appjson"
-        echo 'Source: {"Title":"'$title'","Location":"http://weboslives.eu/AppPackages/'$filename_appjson'","Feed":"WOSA","LastUpdated":"'$modified_date'",","Category":"'$category'","Homepage":"'$homeURL_appjson'","Icon":"http://appimages.webosarchive.com/'$appIcon'","FullDescription":"'$description'","Screenshots":['$screenshot_appjson'],"Countries":["'$locale_appjson'"],"Languages":["'$locale_appjson'"],"License":"'$licenseURL_appjson'","DeviceCompatibility":['$devices']}'
-        echo "Description: $title"
+        echo 'Source: {"Title":"'$title'","Location":"http://weboslives.eu/AppPackages/'$filename_appjson'","Source":"http://weboslives.eu/AppPackages/'$filename_appjson'","Type":"Application","Feed":"WOSA","LastUpdated":"'$modified_date'",","Category":"'$category'","Homepage":"'$homeURL_appjson'","Icon":"http://appimages.webosarchive.com/'$appIcon'","FullDescription":"'$description'","Screenshots":['$screenshot_appjson'],"Countries":["'$locale_appjson'"],"Languages":["'$locale_appjson'"],"License":"'$licenseURL_appjson'","DeviceCompatibility":['$devices']}'
+	echo "Description: $title"
         echo ""
 done
