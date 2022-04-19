1f7a177eed6b711323daa73d63437d2d92916c0b
Adam Maracz <adam.maracz@me.com>
2022-04-19 20:50:00 +0200

    Changed source file for descriptions + fixed typos in Source:

--- convert.sh
+++ convert.sh
@@ -26,16 +26,16 @@
 for id in `jq '.[].id' archivedAppData.json`; do 
         publicApplicationId_appjson=`jq .publicApplicationId webos-catalog-metadata-main/$id.json | sed 's/\"//g'`
         version_appjson=`jq .version webos-catalog-metadata-main/$id.json | sed 's/\"//g'`
-        category=`jq '.[] | select(.id=='$id') | .category' archivedAppData.json | sed 's/\"//g'`
-        author=`jq '.[] | select(.id=='$id') | .author' archivedAppData.json | sed 's/\"//g'`
+        category=`jq '.[] | select(.id=='$id') | .category' archivedAppData.json | sed 's/\"//g' | tail -1`
+        author=`jq '.[] | select(.id=='$id') | .author' archivedAppData.json | sed 's/\"//g' | tail -1`
         appSize_appjson=`jq .appSize webos-catalog-metadata-main/$id.json | sed 's/\"//g'`
         filename_appjson=`jq .filename webos-catalog-metadata-main/$id.json | sed 's/\"//g'`
-        title=`jq '.[] | select(.id=='$id') | .title' archivedAppData.json | sed 's/\"//g'`
+        title=`jq '.[] | select(.id=='$id') | .title' archivedAppData.json | sed 's/\"//g' | tail -1`
         homeURL_appjson=`jq .homeURL webos-catalog-metadata-main/$id.json | sed 's/\"//g'`
-        lastModifiedTime_appjson=`jq .lastModifiedTime webos-catalog-metadata-main/$id.json | sed 's/\"//g'`
-        modified_date=`date -j -f "%Y-%m-%dT%H:%M:%S" $lastModifiedTime_appjson +%s 2>/dev/null`
-        appIcon=`jq '.[] | select(.id=='$id') | .appIcon' archivedAppData.json | sed 's/\"//g'`
-        description=`jq '.[] | select(.id=='$id') | .description' archivedAppData.json | sed 's/\"//g'`
+	lastModifiedTime_appjson=`jq .lastModifiedTime webos-catalog-metadata-main/$id.json | sed 's/\"//g'`
+	modified_date=`date -j -f "%Y-%m-%dT%H:%M:%S" $lastModifiedTime_appjson +%s 2>/dev/null`
+        appIcon=`jq '.[] | select(.id=='$id') | .appIcon' archivedAppData.json | sed 's/\"//g' | tail -1`
+        description_appjson=`jq .description webos-catalog-metadata-main/$id.json | sed 's/\"//g' | head -1`
         screenshot_appjson=`jq .[] webos-catalog-metadata-main/$id.json | jq .[].screenshot 2>/dev/null | grep -v ^null | sed 's/^\"/\"http\:\/\/appimages.webosarchive.com\//g' | tr '\n' ',' | sed 's/,$//g'`
         locale_appjson=`jq .locale webos-catalog-metadata-main/$id.json | sed 's/\"//g'`
         licenseURL_appjson=`jq .licenseURL webos-catalog-metadata-main/$id.json | sed 's/\"//g'`
@@ -47,7 +47,7 @@ for id in `jq '.[].id' archivedAppData.json`; do
         echo "Maintainer: $author"
         echo "Size: $appSize_appjson"
         echo "Filename: $filename_appjson"
-        echo 'Source: {"Title":"'$title'","Location":"http://weboslives.eu/AppPackages/'$filename_appjson'","Source":"http://weboslives.eu/AppPackages/'$filename_appjson'","Type":"Application","Feed":"WOSA","LastUpdated":"'$modified_date'",","Category":"'$category'","Homepage":"'$homeURL_appjson'","Icon":"http://appimages.webosarchive.com/'$appIcon'","FullDescription":"'$description'","Screenshots":['$screenshot_appjson'],"Countries":["'$locale_appjson'"],"Languages":["'$locale_appjson'"],"License":"'$licenseURL_appjson'","DeviceCompatibility":['$devices']}'
+        echo 'Source: {"Title":"'$title'","Location":"http://weboslives.eu/AppPackages/'$filename_appjson'","Source":"http://weboslives.eu/AppPackages/'$filename_appjson'","Type":"Application","Feed":"WOSA","LastUpdated":"'$modified_date'","Category":"'$category'","Homepage":"'$homeURL_appjson'","Icon":"http://appimages.webosarchive.com/'$appIcon'","FullDescription":"'$description'","Screenshots":['$screenshot_appjson'],"Countries":["'$locale_appjson'"],"Languages":["'$locale_appjson'"],"License":"'$licenseURL_appjson'","DeviceCompatibility":['$devices']}'
         echo "Description: $title"
         echo ""
 done
