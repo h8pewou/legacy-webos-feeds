d1be831981b44716ffa2e7c85558f673775e8778
Adam Maracz <adam@weboslives.eu>
2023-07-08 15:47:30 +0200

    Complete rewrite of the earlier bash script

---
+++ convert.ps1
@@ -0,0 +1,108 @@
+# Use try-catch block to handle curl errors
+try {
+    # Invoke the web request to download main museum archive and convert the response from JSON
+    $archivedAppData = (curl "https://raw.githubusercontent.com/webOSArchive/webos-catalog-backend/main/archivedAppData.json" | ConvertFrom-Json)
+}
+catch {
+    # Write the error message to the console
+    Write-Error $_.Exception.Message
+    # Exit the script with a non-zero exit code
+    exit 1
+}
+
+Write-Host "Generating Preware feed: "
+$preware_feed = ($archivedAppData | ForEach-Object -ThrottleLimit 8 -Parallel {
+    # Define the WOSA archive URLs and device variables
+    $catalog_metadata_url = "http://weboslives.eu/feeds/wosa/webos-catalog-metadata"
+    $museum_app_url = "http://museum.weboslives.eu/AppPackages/"
+    $museum_img_url = "http://museum.weboslives.eu/AppImages/"
+    $devices = "Pixi","Pre","Pre2","Pre3","Veer","TouchPad","LuneOS"
+    # Define App ID variable as a string
+    $archivedAppData_id = $_.id.ToString()
+    # Construct the full URL to the app metadata json file
+    $app_metadata_url = ($catalog_metadata_url + "/" + $archivedAppData_id + ".json")
+    try {
+        # Retrieve the app metadata from the catalog metadata url
+        $app_metadata = (Invoke-WebRequest $app_metadata_url | Select-Object -ExpandProperty Content | ConvertFrom-Json)
+    }
+    catch {
+        # Handle the error if metadata retrieval fails
+        $error_message = "Failed to retrieve metadata for AppID: $archivedAppData_id. Error: $($_.Exception.Message)"
+        Write-Error $error_message
+        continue
+    }
+    
+    # Use try-catch block to handle last modified date errors
+    try {
+        # Get the last modified time from the app metadata
+        $lastmodified = ($app_metadata | Select-Object -ExpandProperty lastModifiedTime)
+        # Check if the last modified time is empty or not a valid date
+        if ([string]::IsNullOrEmpty($lastmodified) -or -not (Get-Date -Date $lastmodified -UFormat %s -ErrorAction SilentlyContinue)) {
+            # Set a dummy date as the last modified time
+            $lastmodified = "01/01/1970"
+            # Write a warning message to the console
+            Write-Progress -Activity "  Processing" -Status "The last modified time is empty or invalid. Setting a dummy date. AppID: $archivedAppData_id"
+        }
+        # Convert the last modified time to Unix timestamp
+        $lastupdated = (Get-Date -Date $lastmodified -UFormat %s)
+    }
+    catch {
+        # Write the error message to the console
+        $error_message = ($_.Exception.Message + " AppID: " + $archivedAppData_id)
+        Write-Error $error_message
+    }
+    # Construct the source location URL
+    $source_location = ($museum_app_url + $app_metadata.filename)
+    # Construct the icon URL
+    $iconurl = ($museum_img_url + $_.appIcon)
+    # Construct screenshot URLs
+    $images = ($app_metadata | Select-Object -ExpandProperty images)
+    $screenshots = ($images.PSObject.Properties.Value.screenshot | ForEach-Object{ $museum_img_url + $_ })
+    # Construct device compatibility
+    $archivedAppData_all = $_
+    $devicecompatibility = ($devices | foreach-object {
+        $device = $archivedAppData_all
+        If($device.$_ -eq "True") { $_ }
+    })
+    # Define a function to remove special characters
+    function Remove-SpecialChars {
+        param (
+          [string]$InputString
+        )
+        # Use regular expression to replace any non-alphanumeric character except . , - with an empty string
+        $OutputString = $InputString -replace '[^\w .,-]', ''
+        # Return the output string
+        return $OutputString
+    }
+    # Remove special characters from the description
+    $description = Remove-SpecialChars -InputString $app_metadata.description
+
+    # Construct the source table
+    $Source_Table = @{
+        Title = $_.title
+        Location = $source_location
+        Source = $source_location
+        Type = "Application"
+        Feed = "WOSA"
+        LastUpdated = $lastupdated
+        Category = $_.category
+        Homepage = $app_metadata.homeURL
+        Icon = $iconurl
+        FullDescription = $description
+        Screenshots = $screenshots
+        Countries = $app_metadata.locale
+        Languages = $app_metadata.locale
+        License = $app_metadata.licenseURL
+        DeviceCompatibility = $devicecompatibility
+    }
+    $Source = ($Source_Table | ConvertTo-Json -Compress)
+    # Output all information obtained above
+    Write-Output ("Package: " + $archivedAppData_id + "`nVersion: " + $app_metadata.Version + "`nSection: " + $_.Category + "`nArchitecture: all" + "`nMaintainer: " + $_.Author + "`nSize: " + $app_metadata.appSize + "`nFilename: " + $app_metadata.originalFileName + "`nSource: " + $Source + "`nDescription: " + $_.title + "`n")
+    # Display progress on screen
+    Write-Progress -Activity "  Processing" -Status "$archivedAppData_id "
+})
+
+# Write the Preware feed to the Packages file
+$preware_feed | Out-File Packages
+
+Write-Host "`nCompleted."
